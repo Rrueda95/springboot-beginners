ecd8b1dbb47e25852f4c3787c7e0ec61645ff7a3
Raul Peña <raul.pena@gmail.com>
2020-03-07 13:25:14 -0300

    Added aop service.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,6 @@
 INSERT INTO country (cou_name) VALUES ('Argentina');
 INSERT INTO country (cou_name) VALUES ('Venezuela');
 INSERT INTO country (cou_name) VALUES ('Uruguay');
+
+INSERT INTO student (stu_dni, stu_last_name, stu_name, stu_postal, stu_street, stu_street_number, cou_id)
+VALUES ('123', 'Simpson', 'Homer', '11765', 'Corrientes', 5000, 1);
